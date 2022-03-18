2c1e8b5248be497649c401ee7da33ba8d47b06ef
Gus <gus.stobbs@gmail.com>
2022-03-18 16:27:40 -0500

    Completed Part 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,14 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Please enter a location.")
     @Size(max = 100, message = "Please enter a shorter location name.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer(String location) {
+        this.location = location;
+    }
 
     public Employer() {
     }
